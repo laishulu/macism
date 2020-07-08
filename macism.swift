caa9a18958f09afeae1dbee60fc33abdd438d92c
Mao <code@mao.com>
2020-07-08 17:28:57 +0800

    modify default sleep to 5000 micro seconds

--- macism.swift
+++ macism.swift
@@ -46,7 +46,7 @@ class InputSource: Equatable {
 
 class InputSourceManager {
     static var inputSources: [InputSource] = []
-    static var uSeconds: UInt32 = 60000
+    static var uSeconds: UInt32 = 5000
 
     static func initialize() {
         let inputSourceNSArray = TISCreateInputSourceList(nil, false)
