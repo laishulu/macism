9792767449ae21b3ab1e70675b76cfda0325e465
Mao <code@mao.com>
2020-07-11 20:56:13 +0800

    clean codes

--- macism.swift
+++ macism.swift
@@ -173,15 +173,17 @@ if CommandLine.arguments.count == 1 {
     let currentSource = InputSourceManager.getCurrentSource()
     print(currentSource.id)
 } else {
-    if CommandLine.arguments.count == 3 {
-       InputSourceManager.uSeconds = UInt32(CommandLine.arguments[2])!
-    }
-    let checkOptPrompt = kAXTrustedCheckOptionPrompt.takeUnretainedValue() as NSString
+    let checkOptPrompt = kAXTrustedCheckOptionPrompt.takeUnretainedValue(
+        ) as NSString
     let options = [checkOptPrompt: true]
-    let isAppTrusted = AXIsProcessTrustedWithOptions(options as CFDictionary?);
+    let isAppTrusted = AXIsProcessTrustedWithOptions(options as CFDictionary?)
     if(isAppTrusted == true) {
-        let dstSource = InputSourceManager
-        .getInputSource(name: CommandLine.arguments[1])
+        let dstSource = InputSourceManager.getInputSource(
+            name: CommandLine.arguments[1]
+        )
+        if CommandLine.arguments.count == 3 {
+            InputSourceManager.uSeconds = UInt32(CommandLine.arguments[2])!
+        }
         dstSource.select()
     }
 }
